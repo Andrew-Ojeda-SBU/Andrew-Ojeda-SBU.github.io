1710582d80dec7fb31b4dabe94ba25b647231562
Andrew Ojeda <andrew.ojeda@stonybrook.edu>
2022-10-28 14:55:06 -0400

    Updates

---
+++ precache-manifest.d1cf7c83ac549032b64aa55e3803744b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9db186b6b69bf8a0e3728595268356f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c8d13900b3c1daf545f",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "eb962dd222dbbe468870",
+    "url": "/static/css/main.e58cf2aa.chunk.css"
+  },
+  {
+    "revision": "5c8d13900b3c1daf545f",
+    "url": "/static/js/2.0330be90.chunk.js"
+  },
+  {
+    "revision": "923a93445da4455d8e7e236ec4fa5442",
+    "url": "/static/js/2.0330be90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb962dd222dbbe468870",
+    "url": "/static/js/main.ea18de80.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.bc841d31ed3dbff9034d5ac5887c9dc9.js"
+  "/precache-manifest.d1cf7c83ac549032b64aa55e3803744b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
