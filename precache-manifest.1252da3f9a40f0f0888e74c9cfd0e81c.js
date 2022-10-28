1714bff0684e137a145cf2d2c6681962af2fc98a
Andrew Ojeda <andrew.ojeda@stonybrook.edu>
2022-10-28 14:14:45 -0400

    Updates

---
+++ precache-manifest.1252da3f9a40f0f0888e74c9cfd0e81c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a983ae235519679fe94b6b4392a20f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c8d13900b3c1daf545f",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "08979edb6c98fce1d41e",
+    "url": "./static/css/main.e58cf2aa.chunk.css"
+  },
+  {
+    "revision": "5c8d13900b3c1daf545f",
+    "url": "./static/js/2.0330be90.chunk.js"
+  },
+  {
+    "revision": "923a93445da4455d8e7e236ec4fa5442",
+    "url": "./static/js/2.0330be90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08979edb6c98fce1d41e",
+    "url": "./static/js/main.7ba06a17.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
